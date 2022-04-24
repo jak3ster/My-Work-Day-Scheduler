4b489a179ee30c9cb3eb8a2eed26230e9597a633
Jay A <jautus@hotmail.com>
2022-04-24 00:01:25 -0700

    Added script.js, styles.css, index.html, readme.md, 05-third-party-apis-homework-demo.gif and assets directory structure

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # My-Work-Day-Scheduler
+Simple calendar application
+
+## Description
+
+This homework helped me create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app runs in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+The activities this week help familiarize moment.js library to work with date and time, jquery elements, UI widgets and interactions, click events, form elements, DOM traversal, event delegation, google fonts, bootstrap components, layout, utilities and extends.
+
+## Installation
+
+1. Download or clone repository
+2. Open the main page (index.html) on your browser to view webpage
+3. Use a text editor to view all coding, Visual Studio Code (recommended editor)
+
+## Usage
+
+1. Once My-Work-Day-Scheduler page launches
+2. The current day is displayed at the top of the calendar
+3. Scroll down and presented with timeblocks for standard business hours
+4. Viewing the timeblocks for that day, each timeblock is color coded to indicate whether it is in the past, present, or future
+5. Click into a timeblock and enter an event
+6. Click the save button for that timeblock and the text for that event is saved in local storage
+7 . Refresh the page and the saved events persist
+
+## Features
+
+* HTML
+* CSS
+* jQuery
+* Bootstrap
+* Moment.js
+
+## Demo
+
+![alt text](./assets/images/my-work-day-scheduler-demo.gif)
+
+## Website & Repository
+
+* Deployed Link: https://jak3ster.github.io/My-Work-Day-Scheduler
+* Github Repository: https://github.com/jak3ster/My-Work-Day-Scheduler
---
+++ assets/js/script.js
@@ -0,0 +1,107 @@
+//
+// ## Your Task
+// Create a simple calendar application that allows a user to save events for 
+// each hour of the day by modifying starter code. This app will run in the 
+// browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+// You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. 
+// Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+//
+
+//
+// ## Acceptance Criteria
+//
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+// VARIABLES
+
+// FUNCTIONS
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+var dateNow = moment().format("dddd MMMM Do YYYY");
+$("#currentDay").text(dateNow);
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+var workHours = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM']
+var template = "";
+workHours.forEach(function(time, i) {
+    let id = i + 9;
+    template += `
+    <div class="row time-block" id="${id}">
+        <div class="col-1 hour pt-2">${time}</div>
+        <textarea class="col-10 plan"></textarea>
+        <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
+    </div>`
+});
+$(".container").html(template);
+
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+function timeBlockColor() {
+    let hour = moment().hours();
+    let timeBlock = $(".time-block"); 
+    for(let i = 0; i < timeBlock.length; i++) {
+        var currHour = parseInt($(timeBlock[i]).attr("id")); 
+
+        if (currHour > hour) {
+            $(timeBlock[i]).addClass("future");
+        } else if (currHour === hour) {
+            $(timeBlock[i]).addClass("present");
+        } else {
+            $(timeBlock[i]).addClass("past");
+        }
+    }
+}
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+
+    // THEN the text for that event is saved in local storage
+    storeData(time, plan);
+});
+
+// WHEN I refresh the page
+// THEN the saved events persist
+function usePlanner() {
+    $(".hour").each(function () {
+        var currHour = $(this).text();
+        var currPlan = readData(currHour);
+
+        if (currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
+// LOCAL STORAGE FUNCTIONS
+function storeData(key, value){
+    localStorage.setItem(key, JSON.stringify(value));
+}
+function readData(key){
+    return JSON.parse(localStorage.getItem(key)); 
+}
+
+// CALL FUNCTIONS
+timeBlockColor();
+usePlanner();
